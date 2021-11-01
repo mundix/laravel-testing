7a7528657bd50696c9dab8ec9317d4a1ceaaf2e1
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-01 15:48:56 -0400

    All test passed

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -22,65 +22,47 @@ class ProductsTest extends TestCase
         ]);
     }
 
-//    private function create_user()
-//    {
-//        return  User::factory()->make([
-//            'email' => 'ce.pichardo@gmail.com',
-//            'password' => bcrypt('password123')
-//        ]);
-//    }
-
     /**
-     * A basic test example.
-     *
-     * @return void
-     */
+     * This test must be logged to access
+    */
 //    public function test_homepage_contains_empty_products_table()
 //    {
 //        $response = $this->get('/');
-//
 //        $response->assertStatus(200);
 //        $response->assertSee('No Products Found');
 //    }
 
-//    public function test_homepage_contains_non_empty_products_table()
-//    {
-//        $product = Product::create([
-//            'name' => 'Product 1',
-//            'price' => 99.99
-//        ]);
-//        $response = $this->actingAs($this->user)->get('/');
-//        $response->assertStatus(200);
-//
-//        $response->assertDontSee('No Products Found');
-//        $response->assertSee($product->name);
-//
-//        $view_products = $response->viewData('products');
-//        $this->assertEquals($product->name, $view_products->first()->name);
-//    }
+    public function test_homepage_contains_non_empty_products_table()
+    {
+        $product = Product::create([
+            'name' => 'Product 1',
+            'price' => 99.99
+        ]);
+        $response = $this->actingAs($this->user)->get('/products');
+        $response->assertStatus(200);
 
-//    public function test_pagination_products_table_doesnt_show_11th_record()
-//    {
-//        for ($i = 1; $i <= 11; $i++) {
-//            $product = Product::create([
-//                'name' => 'Product' . $i,
-//                'price' => rand(10, 99)
-//            ]);
-//        }
+        $response->assertDontSee('No Products Found');
+        $response->assertSee($product->name);
 
-//        $products = Product::factory()->count(3)->make();
-//
-//        $response = $this->actingAs($this->user)->get('/');
-//
-//        $response->assertDontSee($products->last()->name);
-//    }
+        $view_products = $response->viewData('products');
+        $this->assertEquals($product->name, $view_products->first()->name);
+    }
 
-//    public function test_admin_can_see_product_create_button()
-//    {
-//        $admin_user = User::factory()->make();
-//
-//        $response = $this->actingAs($admin_user)->get('products');
-//        $response->assertStatus(200);
-//        $response->assertDontSee('Add new product');
-//    }
+    public function test_pagination_products_table_doesnt_show_11th_record()
+    {
+        $products = Product::factory()->count(11)->make();
+
+        $response = $this->actingAs($this->user)->get('/');
+
+        $response->assertDontSee($products->last()->name);
+    }
+
+    public function test_admin_can_see_product_create_button()
+    {
+        $admin_user = User::factory()->make();
+
+        $response = $this->actingAs($admin_user)->get('products');
+        $response->assertStatus(200);
+        $response->assertDontSee('Add new product');
+    }
 }
