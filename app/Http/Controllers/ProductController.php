5c7203f39bbe15ddd24fc7e54b672b27a76553f8
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-01 16:29:06 -0400

    Settle testing database and enviroment

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,7 +36,8 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Product::create($request->all());
+        return redirect()->route('products.index');
     }
 
     /**
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -19,19 +19,15 @@
                         <form action="{{route('products.store')}}" method="post"  >
                             @csrf
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <label for="product_name">Name</label>
+                                <input type="text" class="form-control" id="product_name" aria-describedby="nameHelp" name="name">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <label for="product_price">Price</label>
+                                <input type="text" class="form-control" id="product_price" aria-describedby="priceHelp" name="price">
                             </div>
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+
+                            <button type="submit" class="btn btn-primary">Create</button>
                         </form>
                 </div>
 
